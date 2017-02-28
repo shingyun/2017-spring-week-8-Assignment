05cba54fd3419c35ab0d5859689efbb524c48940
siqister <siqi.zhu@gmail.com>
2017-02-27 22:47:43 -0500

    First commit

---
+++ public/1-canvas-basics/app.js
@@ -0,0 +1,25 @@
+//Part 1: Append a <canvas> element under <div id='plot1'> with the same width and height as the container element
+//Store drawing context as a variable
+//Hint: use canvas.getContext('2d');
+
+
+//Part 2: Draw a gray background, with fillStyle = 'rgb(250,250,250)'
+
+
+//Part 3: Draw a x and y grid, spaced 50px apart, with strokeStyle = 'rgb(180,180,180)'
+//Hint: use context2D.beginPath and context2D path commands within two for... loops
+
+
+//Part 4: Draw a filled red rectangle at (50,50), with width = 50 and height = 50
+//Draw a rectangle with red border at (150,50), with width = 50 and height = 50
+//Hint: use context2D.fillRect and context2D.strokeRect
+
+
+// Part 5: Draw a series of circles and lines using the context2D path commands, as shown
+
+
+// Part 6: Label each circle with coordinates
+// Hint: context2D.fillText
+
+
+// Part 7: append a new <canvas> element under <div id='plot2'>, and copy the content of the first canvas onto it
